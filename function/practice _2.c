1a44fde088f92d4e89e9d7717f9076f0659d8916
guruppriyan <gurusmr17@gmail.com>
2025-07-20 18:36:18 +0530

    practice , practice_2.c

---
+++ function/practice _2.c
@@ -0,0 +1,15 @@
+// functions with return type without parameter 
+# include<stdio.h>
+int myNum () {
+    return 22/7 ;
+}
+
+int main() {
+    int x=12;
+    int y=5;
+    int product = x*y ;
+    int answer= myNum ;
+    printf ("%d" , product) ; 
+    return x/y ;
+
+}
---
+++ function/practice.c
@@ -0,0 +1,13 @@
+// function with parameter but without return value
+# include<stdio.h>
+void myNum (int x , int z) {
+    printf ("%d",x+z ) ;
+} 
+
+int main () {
+    int a= 5;
+    int b= 12;
+    int product =a*b;
+    printf("%d", product) ;
+
+}
